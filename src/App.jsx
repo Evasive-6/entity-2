5a1f3c30d25b4a748229c2da78eb9f208387338b
Evasive-6 <albinshiju285@gmail.com>
2025-02-20 14:52:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-
+// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import AddMovieForm from './components/AddMovieForm';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <div className="app-container">
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/add-movie" element={<AddMovieForm />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
